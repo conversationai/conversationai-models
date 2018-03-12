5776e4bbf1c1db9e00344b00f9ab863b3c1c6acf
nthain <nthain@google.com>
2018-03-12 11:43:34 -0400

    Only process figshare data if necessary.

--- attention-codelab/process_figshare.py
+++ attention-codelab/process_figshare.py
@@ -7,7 +7,18 @@ import os
 
 DEFAULT_DATA_DIR = 'data/'
 
-def prepare_figshare(input_data_dir = DEFAULT_DATA_DIR, output_data_dir = DEFAULT_DATA_DIR):
+def process_figshare(input_data_dir = DEFAULT_DATA_DIR, output_data_dir = DEFAULT_DATA_DIR):
+    
+    already_exist = True
+    for split in ['train', 'test', 'dev']:
+        if not os.path.isfile(os.path.join(output_data_dir, 'wiki_%s.csv' % split)):
+            already_exist = False
+            
+    if already_exist:
+        print('Processed files already exist.')
+        return
+
+    print('Processing files...', end = '')
     toxicity_annotated_comments = pd.read_csv(os.path.join(input_data_dir, 'toxicity_annotated_comments.tsv'), sep = '\t')
     toxicity_annotations = pd.read_csv(os.path.join(input_data_dir, 'toxicity_annotations.tsv'), sep = '\t')
 
@@ -26,6 +37,8 @@ def prepare_figshare(input_data_dir = DEFAULT_DATA_DIR, output_data_dir = DEFAUL
 
     for split in wiki_splits:
         wiki_splits[split].to_csv(os.path.join(output_data_dir, 'wiki_%s.csv' % split), index=False)
-        
+    print('Done!')
+
+# TODO(nthain): Add input and output dirs as flags.
 if __name__ == "__main__":
-    prepare_figshare()
+    process_figshare()
