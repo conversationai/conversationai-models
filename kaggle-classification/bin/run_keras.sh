a148bbfc7c2cb477831bf0928d475b8b0ed51d9e
jjtan <1111780+jjtan@users.noreply.github.com>
2018-05-11 16:21:28 -0400

    Simplify attention + fix run_keras script to handle empty comet_key

--- kaggle-classification/bin/run_keras.sh
+++ kaggle-classification/bin/run_keras.sh
@@ -27,20 +27,20 @@ echo "Writing to $OUTPUT_PATH"
 
 # Remote
 gcloud ml-engine jobs submit training ${JOB_NAME}_${DATE} \
-    --job-dir $OUTPUT_PATH \
-    --runtime-version 1.4 \
-    --module-name keras_trainer.model \
-    --package-path keras_trainer \
-    --region $REGION \
-    --verbosity debug \
-    --config ${HPARAM_CONFIG} \
+    --job-dir=$OUTPUT_PATH \
+    --runtime-version=1.4 \
+    --module-name=keras_trainer.model \
+    --package-path=keras_trainer \
+    --region=$REGION \
+    --verbosity=debug \
+    --config=${HPARAM_CONFIG} \
     -- \
-    --train_path ${INPUT_PATH}/train.csv \
-    --test_path ${INPUT_PATH}/validation.csv \
-    --embeddings_path ${INPUT_PATH}/glove.6B/glove.6B.100d.txt \
-    --log_path ${LOG_PATH} \
-    --comet_key ${COMET_KEY} \
-    --model_type rnn
+    --train_path=${INPUT_PATH}/train.csv \
+    --test_path=${INPUT_PATH}/validation.csv \
+    --embeddings_path=${INPUT_PATH}/glove.6B/glove.6B.100d.txt \
+    --log_path=${LOG_PATH} \
+    --comet_key=${COMET_KEY} \
+    --model_type=rnn
 
 echo "You can view the tensorboard for this job with the command:"
 echo ""
