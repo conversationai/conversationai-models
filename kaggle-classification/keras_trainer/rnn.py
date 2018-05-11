b920bc6085f8798c0938b4e79236e6537d15d63a
jjtan <1111780+jjtan@users.noreply.github.com>
2018-05-11 11:07:11 -0400

    Formatting + Review corrections

--- kaggle-classification/keras_trainer/rnn.py
+++ kaggle-classification/keras_trainer/rnn.py
@@ -10,7 +10,7 @@ from keras_trainer import base_model
 from keras_trainer.custom_metrics import auc_roc
 
 
-class RNN(base_model.BaseModel):
+class RNNModel(base_model.BaseModel):
   """ RNN
 
   hparams:
@@ -30,17 +30,18 @@ class RNN(base_model.BaseModel):
         self.hparams.embedding_dim,
         weights=[self.embeddings_matrix],
         input_length=self.hparams.sequence_length,
-        trainable=self.hparams.train_embedding)(I)
+        trainable=self.hparams.train_embedding)(
+            I)
     X = GRU(128, return_sequences=False)(E)
     X = Dense(128, activation='relu')(X)
     X = Dropout(self.hparams.dropout_rate)(X)
     Output = Dense(6, activation='sigmoid')(X)
 
     model = Model(inputs=I, outputs=Output)
-    model.compile(optimizer='rmsprop',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy', auc_roc])
+    model.compile(
+        optimizer='rmsprop',
+        loss='binary_crossentropy',
+        metrics=['accuracy', auc_roc])
 
     print(model.summary())
     return model
-
