d2d7a2cc8cf25278320d3bf88c4c08e1a40888c3
jjtan <1111780+jjtan@users.noreply.github.com>
2018-06-04 15:27:51 -0400

    Get tf_trainer working on ml engine

--- kaggle-classification/tf_trainer/run.py
+++ kaggle-classification/tf_trainer/run.py
@@ -17,41 +17,29 @@ import tensorflow as tf
 
 from typing import Dict
 
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("train_path", "local_data/train.tfrecord",
+                    "Path to the training data TFRecord file.")
+flags.DEFINE_string("validate_path", "local_data/validation.tfrecord",
+                    "Path to the validation data TFRecord file.")
+flags.DEFINE_string("embeddings_path", "local_data/glove.6B/glove.6B.100d.txt",
+                    "Path to the embeddings file.")
+flags.DEFINE_string("--text_feature_name", "comment_text",
+                    "Feature name of the text feature.")
+
 # TODO: Missing fields are not handled properly yet.
 LABELS = {
     "frac_neg": tf.float32,
-    #"frac_very_neg": tf.float32,
+    "frac_very_neg": tf.float32,
 }  # type: Dict[str, tf.DType]
 
 
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      "--train_path",
-      type=str,
-      default="local_data/train.tfrecord",
-      help="Path to the training data TFRecord file.")
-  parser.add_argument(
-      "--validate_path",
-      type=str,
-      default="local_data/validation.tfrecord",
-      help="Path to the validation data TFRecord file.")
-  parser.add_argument(
-      "--embeddings_path",
-      type=str,
-      default="local_data/glove.6B/glove.6B.100d.txt",
-      help="Path to the embeddings file.")
-  parser.add_argument(
-      "--text_feature_name",
-      type=str,
-      default="comment_text",
-      help="Feature name of the text feature.")
-
-  args = parser.parse_args()
-  train_path = types.Path(args.train_path)
-  validate_path = types.Path(args.validate_path)
-  embeddings_path = types.Path(args.embeddings_path)
-  text_feature_name = args.text_feature_name
+  train_path = types.Path(FLAGS.train_path)
+  validate_path = types.Path(FLAGS.validate_path)
+  embeddings_path = types.Path(FLAGS.embeddings_path)
+  text_feature_name = FLAGS.text_feature_name
 
   preprocessor = text_preprocessor.TextPreprocessor(embeddings_path)
   dataset = tfrecord_input.TFRecordInput(
@@ -76,4 +64,4 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+  tf.app.run()
