46d4ec63b505bb70624d2e5d498e642128136cdc
jjtan <1111780+jjtan@users.noreply.github.com>
2018-06-08 16:05:08 -0400

    Remove setup for other modules

--- experiments/setup.py
+++ experiments/setup.py
@@ -1,26 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = [
-    'tflearn>=0.3.2', 'Keras==2.1.5', 'h5py==2.7.1', 'comet-ml==1.0.8',
-    'nltk>=3.3'
-]
-
-setup(
-    name='trainer',
-    version='0.1',
-    install_requires=REQUIRED_PACKAGES,
-    packages=find_packages(),
-    include_package_data=True,
-    description='tflearn.')
-
-setup(
-    name='keras_trainer',
-    version='0.1',
-    install_requires=REQUIRED_PACKAGES,
-    packages=find_packages(),
-    include_package_data=True,
-    description='tflearn.')
+REQUIRED_PACKAGES = ['Keras==2.1.5', 'comet-ml==1.0.8', 'nltk>=3.3']
 
 setup(
     name='tf_trainer',
@@ -28,4 +9,4 @@ setup(
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     include_package_data=True,
-    description='tflearn.')
+    description='Common Keras model + TF Estimator modelling framework.')
