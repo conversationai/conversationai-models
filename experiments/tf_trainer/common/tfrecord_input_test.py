f20e9710a74c90a0c7d2ff2814fa8406af70392a
nthain <nthain@google.com>
2018-07-03 09:05:46 -0230

    adding test for rounding

--- experiments/tf_trainer/common/tfrecord_input_test.py
+++ experiments/tf_trainer/common/tfrecord_input_test.py
@@ -14,41 +14,56 @@ import tensorflow as tf
 
 class TFRecordInputTest(tf.test.TestCase):
 
-  def test_read_tf_example(self):
+
+  def setUp(self): 
     ex = tf.train.Example(
-        features=tf.train.Features(
-            feature={
-                "label":
-                    tf.train.Feature(
-                        float_list=tf.train.FloatList(value=[0.8])),
-                "comment":
-                    tf.train.Feature(
-                        bytes_list=tf.train.BytesList(
-                            value=["Hi there Bob".encode("utf-8")]))
-            }))
-    ex_tensor = tf.convert_to_tensor(ex.SerializeToString(), dtype=tf.string)
+      features=tf.train.Features(
+          feature={
+              "label":
+                  tf.train.Feature(
+                      float_list=tf.train.FloatList(value=[0.8])),
+              "comment":
+                  tf.train.Feature(
+                      bytes_list=tf.train.BytesList(
+                          value=["Hi there Bob".encode("utf-8")]))
+                  }))
+    self.ex_tensor = tf.convert_to_tensor(ex.SerializeToString(), dtype=tf.string)
 
-    word_to_idx = {"Hi": 12, "there": 13}
-    unknown_token = 999
+    self.word_to_idx = {"Hi": 12, "there": 13}
+    self.unknown_token = 999
 
-    def preprocessor(text):
-      return tf.py_func(
-          lambda t: np.asarray([word_to_idx.get(x, unknown_token) for x in t.decode().split(" ")]),
-          [text], tf.int64)
+  def preprocessor(self, text):
+    return tf.py_func(
+        lambda t: np.asarray([self.word_to_idx.get(x, self.unknown_token) for x in t.decode().split(" ")]),
+        [text], tf.int64)
 
+  def test_TFRecordInput_unrounded(self):
     dataset_input = tfrecord_input.TFRecordInput(
         train_path=None,
         validate_path=None,
         text_feature="comment",
         labels={"label": tf.float32},
-        feature_preprocessor=preprocessor,
+        feature_preprocessor=self.preprocessor,
         round_labels=False)
 
     with self.test_session():
-      features, labels = dataset_input._read_tf_example(ex_tensor)
+      features, labels = dataset_input._read_tf_example(self.ex_tensor)
       self.assertEqual(list(features["comment"].eval()), [12, 13, 999])
       self.assertAlmostEqual(labels["label"].eval(), 0.8)
 
+  def test_TFRecordInput_rounded(self):
+    dataset_input = tfrecord_input.TFRecordInput(
+        train_path=None,
+        validate_path=None,
+        text_feature="comment",
+        labels={"label": tf.float32},
+        feature_preprocessor=self.preprocessor,
+        round_labels=True)
+
+    with self.test_session():
+      features, labels = dataset_input._read_tf_example(self.ex_tensor)
+      self.assertEqual(list(features["comment"].eval()), [12, 13, 999])
+      self.assertEqual(labels["label"].eval(), 1.0)
 
 if __name__ == "__main__":
   tf.test.main()
