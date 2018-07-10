958ecc736bb1fe2ac592cd2b30b89015051ad775
Flavien Prost <fprost@fprost1.nyc.corp.google.com>
2018-07-10 17:14:49 -0400

    Fixing path of deployment

--- experiments/tf_trainer/keras_gru_attention/run.deploy.sh
+++ experiments/tf_trainer/keras_gru_attention/run.deploy.sh
@@ -2,7 +2,9 @@
 
 # Edit these!
 MODEL_NAME=keras_gru_attention
-MODEL_SAVED_PATH=gs://kaggle-model-experiments/${USER}/1531159115
+# By default, the model is the last one from the user.
+MODEL_SAVED_PATH_FOLDER=$(gs://kaggle-model-experiments/tf_trainer_runs/${USER}/${MODEL_NAME}/)
+MODEL_SAVED_PATH=${MODEL_SAVED_PATH_FOLDER}/model_dir
 
 # Create a new model.
 # Will raise an error if the model already exists.
