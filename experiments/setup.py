d2235c10d6d07e37de05ea8a05e74ad596e08cb4
Flavien Prost <fprost@google.com>
2018-07-13 10:16:48 -0400

    Creating shell script for single job + fixing setup

--- experiments/setup.py
+++ experiments/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['Keras==2.2.0', 'comet-ml==1.0.16', 'nltk>=3.3']
+REQUIRED_PACKAGES = ['Keras==2.2.0', 'comet-ml==1.0.16', 'nltk>=3.3', 'typed_ast==1.1.0']
 
 setup(
     name='tf_trainer',
