ebb10330cff0ca8c1344e69e0fa215df30bc8dee
nthain <nthain@google.com>
2018-07-23 16:10:39 -0230

    Modifying setup.py to fix CUDA issue

--- experiments/setup.py
+++ experiments/setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['Keras==2.2.0', 'comet-ml==1.0.16', 'nltk>=3.3', 'typed_ast==1.1.0']
+REQUIRED_PACKAGES = ['Keras==2.2.0', 'comet-ml==1.0.16', 'nltk>=3.3',
+                      'typed_ast==1.1.0', 'tensorflow-gpu==1.5']
 
 setup(
     name='tf_trainer',
