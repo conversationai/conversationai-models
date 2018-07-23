daf5d61af13c393f88622da8c1578036681d3a9f
nthain <nthain@google.com>
2018-07-23 18:21:10 -0230

    undoing setup.py change

--- experiments/setup.py
+++ experiments/setup.py
@@ -1,8 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['Keras==2.2.0', 'comet-ml==1.0.16', 'nltk>=3.3',
-                      'typed_ast==1.1.0', 'tensorflow-gpu==1.5']
+REQUIRED_PACKAGES = ['Keras==2.2.0', 'comet-ml==1.0.16', 'nltk>=3.3', 'typed_ast==1.1.0']
 
 setup(
     name='tf_trainer',
