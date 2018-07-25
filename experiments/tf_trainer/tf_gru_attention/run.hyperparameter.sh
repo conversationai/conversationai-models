ff45a21a22e58d962cade0af6460b998425b19d8
Flavien Prost <fprost@google.com>
2018-07-25 16:44:01 -0400

    TF branch is compatible + change attention

--- experiments/tf_trainer/tf_gru_attention/run.hyperparameter.sh
+++ experiments/tf_trainer/tf_gru_attention/run.hyperparameter.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# This script runs one training job on Cloud MLE.
 
 GCS_RESOURCES="gs://kaggle-model-experiments/resources"
 DATETIME=`date '+%Y%m%d_%H%M%S'`
-MODEL_NAME="keras_gru_attention"
+MODEL_NAME="tf_gru_attention"
 JOB_DIR=gs://kaggle-model-experiments/tf_trainer_runs/${USER}/${MODEL_NAME}/${DATETIME}
 LOCAL_COMET_API_KEY_FILE="comet_api_key.txt"
 REMOTE_COMET_API_KEY_FILE=${GCS_RESOURCES}/${USER}_comet_api_key.txt
@@ -13,17 +12,16 @@ gsutil acl set private ${REMOTE_COMET_API_KEY_FILE} &&
 gcloud ml-engine jobs submit training tf_trainer_${MODEL_NAME}_${USER}_${DATETIME} \
     --job-dir=${JOB_DIR} \
     --runtime-version=1.8 \
-    --scale-tier 'BASIC_GPU' \
     --module-name="tf_trainer.${MODEL_NAME}.run" \
     --package-path=tf_trainer \
-    --python-version "3.5" \
-    --region=us-central1 \
+    --region=us-east1 \
     --verbosity=debug \
+    --config="tf_trainer/${MODEL_NAME}/hparam_config.yaml" \
     -- \
     --train_path="${GCS_RESOURCES}/toxicity_q42017_train.tfrecord" \
     --validate_path="${GCS_RESOURCES}/toxicity_q42017_validate.tfrecord" \
-    --embeddings_path="${GCS_RESOURCES}/google-news/GoogleNews-vectors-negative300.txt" \
+    --embeddings_path="${GCS_RESOURCES}/glove.6B/glove.6B.300d.txt" \
     --model_dir="${JOB_DIR}/model_dir" \
     --comet_key_file="${REMOTE_COMET_API_KEY_FILE}" \
     --comet_team_name="jigsaw" \
-    --comet_project_name="experiments_${USER}"
+    --comet_project_name="experiments"
--- experiments/tf_trainer/tf_gru_attention/run.ml_engine.sh
+++ experiments/tf_trainer/tf_gru_attention/run.ml_engine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script runs one training job on Cloud MLE.
 
 GCS_RESOURCES="gs://kaggle-model-experiments/resources"
 DATETIME=`date '+%Y%m%d_%H%M%S'`
@@ -12,15 +13,17 @@ gsutil acl set private ${REMOTE_COMET_API_KEY_FILE} &&
 gcloud ml-engine jobs submit training tf_trainer_${MODEL_NAME}_${USER}_${DATETIME} \
     --job-dir=${JOB_DIR} \
     --runtime-version=1.8 \
+    --scale-tier 'BASIC_GPU' \
     --module-name="tf_trainer.${MODEL_NAME}.run" \
     --package-path=tf_trainer \
-    --region=us-east1 \
+    --python-version "3.5" \
+    --region=us-central1 \
     --verbosity=debug \
-    --config="tf_trainer/${MODEL_NAME}/hparam_config.yaml" \
     -- \
     --train_path="${GCS_RESOURCES}/toxicity_q42017_train.tfrecord" \
     --validate_path="${GCS_RESOURCES}/toxicity_q42017_validate.tfrecord" \
     --embeddings_path="${GCS_RESOURCES}/glove.6B/glove.6B.300d.txt" \
+    --is_binary_embedding False \
     --model_dir="${JOB_DIR}/model_dir" \
     --comet_key_file="${REMOTE_COMET_API_KEY_FILE}" \
     --comet_team_name="jigsaw" \
