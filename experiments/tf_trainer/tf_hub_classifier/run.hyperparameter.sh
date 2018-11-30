5c7edfecfaee835db5a1f939bd145f4c8fad9290
Mikhail Sushkov <msushkov@google.com>
2018-11-30 14:52:57 -0800

    Adding per-dataset hparam config files for tf-hub and tf_gru models

--- experiments/tf_trainer/tf_hub_classifier/run.hyperparameter.sh
+++ experiments/tf_trainer/tf_hub_classifier/run.hyperparameter.sh
@@ -3,22 +3,62 @@
 GCS_RESOURCES="gs://kaggle-model-experiments/resources"
 DATETIME=`date '+%Y%m%d_%H%M%S'`
 MODEL_NAME="tf_hub_classifier"
-JOB_DIR=gs://kaggle-model-experiments/tf_trainer_runs/${USER}/${MODEL_NAME}/${DATETIME}
 
-gcloud ml-engine jobs submit training tf_trainer_${MODEL_NAME}_${USER}_${DATETIME} \
+if [ "$1" == "civil_comments" ]; then
+    
+    train_path="${GCS_RESOURCES}/civil_comments_data/train_eval_test/train-*.tfrecord"
+    valid_path="${GCS_RESOURCES}/civil_comments_data/train_eval_test/eval-*.tfrecord"
+    train_steps=24000
+    eval_period=800
+    eval_steps=50
+    labels="toxicity"
+
+elif [ "$1" == "toxicity" ]; then
+
+    train_path="${GCS_RESOURCES}/toxicity_q42017_train.tfrecord"
+    valid_path="${GCS_RESOURCES}/toxicity_q42017_validate.tfrecord"
+    train_steps=24000
+    eval_period=800
+    eval_steps=50
+    labels="frac_neg"
+
+elif [ "$1" == "many_communities" ]; then
+
+    train_path="${GCS_RESOURCES}/transfer_learning_data/many_communities/20181105_train.tfrecord"
+    valid_path="${GCS_RESOURCES}/transfer_learning_data/many_communities/20181105_validate.tfrecord"
+    train_steps=100000
+    eval_period=800
+    eval_steps=50
+    labels="removed"
+
+else
+    echo "First positional arg must be one of civil_comments, toxicity, many_communities."
+    return;
+fi
+
+
+MODEL_NAME_DATA=${MODEL_NAME}_$1
+JOB_DIR=gs://kaggle-model-experiments/tf_trainer_runs/${USER}/${MODEL_NAME_DATA}/${DATETIME}
+
+
+gcloud ml-engine jobs submit training tf_trainer_${MODEL_NAME_DATA}_${USER}_${DATETIME} \
     --job-dir=${JOB_DIR} \
     --runtime-version=1.10 \
     --module-name="tf_trainer.${MODEL_NAME}.run" \
     --package-path=tf_trainer \
     --region=us-east1 \
     --verbosity=debug \
-    --config="tf_trainer/${MODEL_NAME}/hparam_config.yaml" \
+    --config="tf_trainer/${MODEL_NAME}/hparam_config_$1.yaml" \
     -- \
-    --train_path="${GCS_RESOURCES}/toxicity_q42017_train.tfrecord" \
-    --validate_path="${GCS_RESOURCES}/toxicity_q42017_validate.tfrecord" \
+    --train_path=$train_path \
+    --validate_path=$valid_path \
     --model_dir="${JOB_DIR}/model_dir" \
-    --is_embedding_trainable False \
-    --train_steps=40000 \
-    --eval_period=800 \
-    --labels=frac_neg,frac_very_neg,sexual_orientation,health_age_disability,gender,religion,rne,obscene,threat,insult,identity_hate,flirtation,sexual_explicit
+    --is_embedding_trainable=False \
+    --train_steps=$train_steps \
+    --eval_period=$eval_period \
+    --eval_steps=$eval_steps \
+    --labels=$labels \
+    --preprocess_in_tf=False
 
+echo "Model dir:"
+echo ${JOB_DIR}/model_dir
