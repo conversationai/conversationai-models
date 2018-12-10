1542fa0cb06b2b1d0a0528af17f04b1483083c08
nthain <nthain@google.com>
2018-12-10 13:05:52 -0330

    adding run scripts to word_label for other datasets and fixing embedding path/dims in other tf_models

--- experiments/tf_trainer/tf_hub_classifier/run.hyperparameter.sh
+++ experiments/tf_trainer/tf_hub_classifier/run.hyperparameter.sh
@@ -55,6 +55,8 @@ gcloud ml-engine jobs submit training tf_trainer_${MODEL_NAME_DATA}_${USER}_${DA
     -- \
     --train_path=$train_path \
     --validate_path=$valid_path \
+    --embeddings_path="${GCS_RESOURCES}/glove.6B/glove.6B.300d.txt" \
+    --embedding_size=300 \
     --model_dir="${JOB_DIR}/model_dir" \
     --is_embedding_trainable=False \
     --train_steps=$train_steps \
--- experiments/tf_trainer/tf_word_label_embedding/run.local.sh
+++ experiments/tf_trainer/tf_word_label_embedding/run.local.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-# Note:
-# We currently use 2 different embeddings:
-# - glove.6B/glove.6B.300d.txt
-# - google-news/GoogleNews-vectors-negative300.txt
-# Glove assumes all words are lowercased, while Google-news handles different casing.
-# As there is currently no tf operation that perform lowercasing, we have the following 
-# requirements:
-# - For google news: Run preprocess_in_tf=True (no lowercasing).
-# - For glove.6B, Run preprocess_in_tf=False (will force lowercasing).
-
-
 GCS_RESOURCES="gs://kaggle-model-experiments/resources"
 
+if [ "$1" == "civil_comments" ]; then
+    
+    train_path="${GCS_RESOURCES}/civil_comments_data/train_eval_test/train-*.tfrecord"
+    valid_path="${GCS_RESOURCES}/civil_comments_data/train_eval_test/eval-*.tfrecord"
+    labels="toxicity"
+    label_dtypes="float"
+
+elif [ "$1" == "toxicity" ]; then
+
+    train_path="${GCS_RESOURCES}/toxicity_q42017_train.tfrecord"
+    valid_path="${GCS_RESOURCES}/toxicity_q42017_validate.tfrecord"
+    labels="frac_neg"
+    label_dtypes="float"
+
+elif [ "$1" == "many_communities" ]; then
+
+    train_path="${GCS_RESOURCES}/transfer_learning_data/many_communities/20181105_train.tfrecord"
+    valid_path="${GCS_RESOURCES}/transfer_learning_data/many_communities/20181105_validate.tfrecord"
+    labels="removed"
+    label_dtypes="int"
+
+else
+    echo "First positional arg must be one of civil_comments, toxicity, many_communities."
+    return;
+fi
+
+
 python -m tf_trainer.tf_word_label_embedding.run \
-  --train_path="${GCS_RESOURCES}/toxicity_q42017_train.tfrecord" \
-  --validate_path="${GCS_RESOURCES}/toxicity_q42017_validate.tfrecord" \
-  --embeddings_path="${GCS_RESOURCES}/glove.6B/glove.6B.100d.txt" \
-  --embedding_size=100 \
+  --train_path=$train_path \
+  --validate_path=$valid_path \
   --model_dir="tf_word_label_embedding_local_model_dir" \
-  --preprocess_in_tf=False
+  --labels=$labels \
+  --label_dtypes=$label_dtypes
