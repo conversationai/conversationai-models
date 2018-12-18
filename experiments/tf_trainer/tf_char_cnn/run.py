51aa57af9069543d06c1927158b6f48ca27a991b
Jeffrey Sorensen <sorenjgoogle.com>
2018-12-18 14:50:01 -0500

    First cut at character model.

---
+++ experiments/tf_trainer/tf_char_cnn/run.py
@@ -0,0 +1,37 @@
+"""Experiments with toxicity, civil_comments, many_communities datasets."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from tf_trainer.common import base_model
+from tf_trainer.common import model_trainer
+from tf_trainer.common import serving_input
+from tf_trainer.common import tfrecord_input
+from tf_trainer.common import types
+from tf_trainer.tf_char_cnn import model as tf_char_cnn
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def main(argv):
+  del argv  # unused
+
+  dataset = tfrecord_input.TFRecordInput()
+
+  model = tf_char_cnn.TFCharCNNModel(dataset.labels())
+
+  trainer = model_trainer.ModelTrainer(dataset, model)
+  trainer.train_with_eval()
+
+  serving_input_fn = serving_input.create_serving_input_fn(
+      text_feature_name=base_model.TEXT_FEATURE_KEY)
+  trainer.export(serving_input_fn)
+
+
+if __name__ == "__main__":
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run(main)
+
