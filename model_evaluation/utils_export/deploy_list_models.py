a42afc953a0c4a5868b4800411f126f70b2d6ba3
Flavien Prost <fprost@google.com>
2019-03-28 17:36:36 -0400

    Script to deploy list of models

--- model_evaluation/utils_export/deploy_list_models.py
+++ model_evaluation/utils_export/deploy_list_models.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Deploys all models that have been saved in a directory."""
+"""Deploys all models that have been saved in a list of directories."""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -130,64 +130,75 @@ def deploy_model_version(project_name, model_name, version_name, model_dir):
   return operation_id
 
 
-def _get_version_name(model_dir):
-  return 'v_{}'.format(os.path.basename(os.path.dirname(model_dir)))
+def _get_version_name(model_dir, go_up_3=True):
+  """Looks for the version_name in the model_directory name.
+  
+  Example: model_dir = gs://.../20190328_103329/model_dir/102500/1553798665/
+    If go_up_3, it will grab '20190328_103329'
+    if not, it will grab '1553798665'.
+  Typically speaking, set up go_up_3=False if a model_run has several exported models."""
+  if go_up_3:
+    name = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(model_dir))))
+    return 'v_{}'.format(os.path.basename(name))
+  else:
+    return 'v_{}'.format(os.path.basename(os.path.dirname(model_dir)))
 
 
-def deploy_all_models(parent_dir, project_name, model_name):
-  """Finds and deploys all models present in the directory.
+def deploy_all_models(list_model_dir, project_name, model_name):
+  """Finds and deploys all models present a list of directories.
 
   Args:
-    parent_dir: Directory to explore.
+    list_model_dir: List of directories to explore.
     project_name: Name of the project.
     model_name: Name of the model. All the model found in the parent_dir will be
       saved within the same main model.
   """
 
-  list_models = get_list_models_to_export(parent_dir)
+  models = []
+  for _model_dir in args.list_model_dir.split(','):
+    models.extend(get_list_models_to_export(_model_dir))
   logging.info('Exploration finished: {} models detected.'.format(
-      len(list_models)))
+      len(models)))
 
-  num_epochs = int(len(list_models) / CLOUD_ML_VERSION_CREATE_QUOTA)
-
+  num_epochs = int(len(models) / CLOUD_ML_VERSION_CREATE_QUOTA)
   for i in range(0, num_epochs + 1):
     indices = range(i * CLOUD_ML_VERSION_CREATE_QUOTA,
                     (i + 1) * CLOUD_ML_VERSION_CREATE_QUOTA)
     operation_id_list = []
     for j in indices:
-      if j >= len(list_models):
+      if j >= len(models):
         break
-      version_name = _get_version_name(list_models[j])
+      version_name = _get_version_name(models[j])
       operation_id = deploy_model_version(
           project_name=project_name,
           model_name=model_name,
           version_name=version_name,
-          model_dir=list_models[j])
+          model_dir=models[j])
       operation_id_list.append(operation_id)
 
     logging.info('Waiting for versions to be deployed...')
     for operation_id in operation_id_list:
       check_version_deployed(operation_id)
 
-  logging.info('DONE. {} models have been deployed'.format(len(list_models)))
+  logging.info('DONE. {} models have been deployed'.format(len(models)))
 
 
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser()
   parser.add_argument(
-      '--parent_dir',
-      help='Name of the parent model directory.',
-      default='gs://kaggle-model-experiments/tf_trainer_runs/fprost/tf_gru_attention/20180917_161053/model_dir/'
+      '--list_model_dir',
+      help='List of the model directory (comma separated).',
+      required=True
   )
   parser.add_argument(
-      '--project_name', help='Name of GCP project.', default='wikidetox')
+      '--project_name', help='Name of GCP project.', default='conversationai-models')
   parser.add_argument(
       '--model_name',
       help='Name of the model on CMLE.',
-      default='tf_gru_attention_continuous')
+      default='tf_test')
   args = parser.parse_args(args=sys.argv[1:])
 
   tf.logging.set_verbosity(tf.logging.INFO)
 
-  deploy_all_models(args.parent_dir, args.project_name, args.model_name)
+  deploy_all_models(args.list_model_dir, args.project_name, args.model_name)
