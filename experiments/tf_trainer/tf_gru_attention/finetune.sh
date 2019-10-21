7f453ce7ad028391890a59019dcfa0ae1d4acc36
Mikhail Sushkov <msushkov@google.com>
2019-10-20 23:34:15 -0700

    Add best valid hparams for finetuned baselines

--- experiments/tf_trainer/tf_gru_attention/finetune.sh
+++ experiments/tf_trainer/tf_gru_attention/finetune.sh
@@ -4,10 +4,6 @@ BASE_PATH="gs://conversationai-models"
 GCS_RESOURCES="${BASE_PATH}/resources"
 
 warm_start_from="gs://conversationai-models/tf_trainer_runs/msushkov/tf_gru_attention_many_communities_40_per_8_shot_glove/20190723_110533/model_dir"
-combined_results_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/results/tf_gru_attention/validation"
-
-train_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/validation_episodes/support/*.tfrecord"
-
 eval_steps=1
 eval_period=5
 
@@ -21,9 +17,23 @@ dropout_rate=0.052541994248873507
 dense_units='128,128'
 gru_units='128'
 
-# original, original/2, original/5, original/10, original*2
-learning_rate_lst=(0.00049418814574477758 0.00024709407 0.00009883762 0.000049418814574477758 0.00098837629)
-train_steps_lst=(5 10 50)
+if [ "$1" == "test" ]; then
+	VALIDATION_OR_TEST="test"
+
+	# Best hparams found on the validation set
+	learning_rate_lst=(0.000049418814574477758)
+	train_steps_lst=(50)
+
+else
+	VALIDATION_OR_TEST="validation"
+
+	# original, original/2, original/5, original/10, original*2, original/20, original/50
+	learning_rate_lst=(0.00049418814574477758 0.00024709407 0.00009883762 0.000049418814574477758 0.00098837629 0.0000247094 0.00000988376)
+	train_steps_lst=(5 10 50)
+fi
+
+combined_results_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/results/tf_gru_attention/$VALIDATION_OR_TEST"
+train_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/${VALIDATION_OR_TEST}_episodes/support/*.tfrecord"
 
 for learning_rate in "${learning_rate_lst[@]}"; do
 	echo "Learning rate:"
@@ -41,7 +51,7 @@ for learning_rate in "${learning_rate_lst[@]}"; do
 		COUNTER=0
 		for train_path in `gsutil ls $train_dir`; do
 			
-			valid_path=${train_path/validation_episodes\/support/validation_episodes\/query}
+			valid_path=${train_path/${VALIDATION_OR_TEST}_episodes\/support/${VALIDATION_OR_TEST}_episodes\/query}
 
 			rm -rf "tf_gru_attention_local_model_dir"
 
@@ -68,11 +78,6 @@ for learning_rate in "${learning_rate_lst[@]}"; do
 			    --tmp_results_path=$tmp_results_path
 
 			COUNTER=$[$COUNTER +1]
-
-			# if [ $COUNTER -eq 2 ]
-			# then
-			#     break;
-			# fi
 		done
 
 		gsutil cp $tmp_results_path $combined_results_dir
--- experiments/tf_trainer/tf_hub_classifier/finetune.sh
+++ experiments/tf_trainer/tf_hub_classifier/finetune.sh
@@ -4,9 +4,6 @@ BASE_PATH="gs://conversationai-models"
 GCS_RESOURCES="${BASE_PATH}/resources"
 
 warm_start_from="gs://conversationai-models/tf_trainer_runs/msushkov/tf_hub_classifier_many_communities_40_per_8_shot/20190723_110557/model_dir"
-combined_results_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/results/tf_hub_classifier/validation"
-
-train_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/validation_episodes/support/*.tfrecord"
 
 eval_steps=1
 eval_period=5
@@ -19,9 +16,23 @@ batch_size=24
 dropout_rate=0.53291173797826941
 dense_units='256,128,64'
 
-# original, original/2, original/5, original/10, original*2
-learning_rate_lst=(6.1924912697697353e-06 0.00000309624 0.00000123849 6.1924912697697353e-07 0.00001238498)
-train_steps_lst=(5 10 50)
+if [ "$1" == "test" ]; then
+	VALIDATION_OR_TEST="test"
+
+	# Best hparams found on the validation set
+	learning_rate_lst=(0.00001238498)
+	train_steps_lst=(50)
+
+else
+	VALIDATION_OR_TEST="validation"
+
+	# original, original/2, original/5, original/10, original*2
+	learning_rate_lst=(6.1924912697697353e-06 0.00000309624 0.00000123849 6.1924912697697353e-07 0.00001238498)
+	train_steps_lst=(5 10 50)
+fi
+
+train_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/${VALIDATION_OR_TEST}_episodes/support/*.tfrecord"
+combined_results_dir="gs://conversationai-models/resources/transfer_learning_data/many_communities_40_per_8_shot/results/tf_hub_classifier/$VALIDATION_OR_TEST"
 
 for learning_rate in "${learning_rate_lst[@]}"; do
 	echo "Learning rate:"
@@ -39,7 +50,7 @@ for learning_rate in "${learning_rate_lst[@]}"; do
 		COUNTER=0
 		for train_path in `gsutil ls $train_dir`; do
 			
-			valid_path=${train_path/validation_episodes\/support/validation_episodes\/query}
+			valid_path=${train_path/${VALIDATION_OR_TEST}_episodes\/support/${VALIDATION_OR_TEST}_episodes\/query}
 
 			rm -rf "tf_hub_classifier_local_model_dir"
 
@@ -63,13 +74,7 @@ for learning_rate in "${learning_rate_lst[@]}"; do
 			    --warm_start_from=$warm_start_from \
 			    --tmp_results_path=$tmp_results_path
 
-
 			COUNTER=$[$COUNTER +1]
-
-			# if [ $COUNTER -eq 2 ]
-			# then
-			#     break;
-			# fi
 		done
 
 		gsutil cp $tmp_results_path $combined_results_dir
